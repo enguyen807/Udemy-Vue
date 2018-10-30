5a2361bd6290957571e5863711ae28536a2faa23
Nguyen <Eric.Nguyen@fleishman.com>
2018-10-30 15:02:24 -0500

    Finished Exercise 10

---
+++ Exercise 9/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button @click="selectedComponent = 'appBlue'" class="btn btn-primary">Load Blue Template</button>
+                <button @click="selectedComponent = 'appGreen'" class="btn btn-success">Load Green Template</button>
+                <button @click="selectedComponent = 'appRed'" class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+                <component :is="selectedComponent">
+                    <p>Default Content</p>
+                </component>
+                <!-- <app-blue>
+                    <h2 slot="blue">Triceratops!</h2>
+                </app-blue>
+                <app-green>
+                    <h2 slot>Dragonzord!</h2>
+                </app-green>
+                <app-red>
+                    <h2 slot="red">Tyrannosaurus!</h2>
+                </app-red> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data: () => {
+            return {
+                selectedComponent: 'appGreen'
+            }
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ Project 2/src/components/Quote.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="panel panel-defaultdiv">
+            <div class="panel-body quote">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      props: ['quotes']  
+    }
+</script>
+
+<style>
+    .panel-body {
+        font-family: 'Poor Story', cursive;
+        font-size: 24px;
+        color: #57A773;
+    }
+
+    .quote {
+        cursor: pointer;
+    }
+
+    .quote:hover {
+        background-color: #F2C078;
+    }
+</style>
