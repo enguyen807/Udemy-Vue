0b2da47e5188849a93c9cfef501bd3373b60652f
Nguyen <Eric.Nguyen@fleishman.com>
2018-10-31 11:09:45 -0500

    Finished S11Exercise

---
+++ S11Exercise/src/App.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <div class="form-group">
+                        <label for="fName">First Name</label>
+                        <input
+                                type="text"
+                                id="fName"
+                                class="form-control"
+                                v-model="userData.fName">
+                    </div>
+                    <div class="form-group">
+                        <label for="lName">Last Name</label>
+                        <input
+                                type="text"
+                                id="lName"
+                                class="form-control"
+                                v-model="userData.lName">
+                    </div>                                        
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input
+                                type="text"
+                                id="email"
+                                class="form-control"
+                                :value="userData.email"
+                                @input="userData.email = $event.target.value">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                class="form-control"
+                                v-model.lazy="userData.password">
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-8 col-md-6">
+                            <app-switch v-model="storeData"></app-switch>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-8 col-md-6">
+                            <button
+                                    class="btn btn-primary"
+                                    @click.prevent="submitted">Submit!
+                            </button>
+                        </div>
+                    </div>                                          
+
+                    <!-- Exercise 3 -->
+                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                    <!-- which still holds the First Name and Last Name Input Field -->
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row" v-if="isSubmitted">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Full Name: {{ userData.fName }} {{ userData.lName }}</p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Store in Database?: {{ storeData }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Switch from './Switch.vue';
+
+    export default {
+        data() {
+            return {
+                userData: {
+                    fName: '',
+                    lName: '',
+                    email: '',
+                    password: ''
+                },
+                storeData: false,
+                isSubmitted: false,
+
+            }
+        },
+        methods: {
+            submitted() {
+                this.isSubmitted = true;
+            }
+        },
+        components: {
+            appSwitch: Switch
+        }        
+    }
+</script>
+
+<style>
+</style>
